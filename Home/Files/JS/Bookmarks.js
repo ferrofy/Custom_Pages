17fa24ffdfce71b114f354284b3b6759523261d8
FerroFy <team.ferrofy@gmail.com>
2026-01-18 04:05:10 +0530

    Bookmarks Update

--- Home/Files/JS/Bookmarks.js
+++ Home/Files/JS/Bookmarks.js
@@ -1,12 +1,16 @@
-
 let Shortcut_Database = [
-    { Title: "Python", Link: "https://www.youtube.com" },
-    { Title: "Linux", Link: "https://www.youtube.com" },
-    { Title: "Java", Link: "https://www.youtube.com" },
-    { Title: "Web Dev", Link: "https://www.youtube.com" },
-    { Title: "SQL", Link: "https://www.youtube.com" },
-    { Title: "Git", Link: "https://www.youtube.com" },
-    { Title: "OS", Link: "https://www.youtube.com" },
+    { Title: "Linux", Link: "https://www.youtube.com/playlist?list=PLJ9KaYo7PXTDJwBDJaikgTsEGnL7KBeyC" },
+    { Title: "OS", Link: "https://www.youtube.com/playlist?list=PLJ9KaYo7PXTCCekllSlLmM3c7sGkxHv4J" },
+    { Title: "Python", Link: "https://www.youtube.com/playlist?list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg" },
+    { Title: "WebDev", Link: "https://www.youtube.com/playlist?list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w" },
+    { Title: "SQL", Link: "https://www.youtube.com/playlist?list=PLJ9KaYo7PXTC66uDTZpqD4ojDA-xt5Krx" },
+    { Title: "Java", Link: "https://www.youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q" },
+    { Title: "C Lang", Link: "https://www.youtube.com/playlist?list=PLu0W_9lII9aiXlHcLx-mDH1Qul38wD3aR" },
+    { Title: "Git", Link: "https://www.youtube.com/playlist?list=PLu0W_9lII9agwhy658ZPA0MTStKUJTWPi" },
+    { Title: "Bash Chunk", Link: "https://www.youtube.com/playlist?list=PLIhvC56v63IKioClkSNDjW7iz-6TFvLwS" },
+    { Title: "Linux Chunk", Link: "https://www.youtube.com/playlist?list=PLIhvC56v63IL2OjFvv_PI0B2yAXGfJLMI" },
+    { Title: "Linux 2 Chunk", Link: "https://www.youtube.com/playlist?list=PLIhvC56v63IJIujb5cyE13oLuyORZpdkL" },
+    { Title: "H@cking Chunk", Link: "https://www.youtube.com/playlist?list=PLIhvC56v63IIJZRa3lzK6IeBQOH_VFjUQ" },
 ];
 
 let Shortcut_Row = document.getElementById("Shortcut_Row");
